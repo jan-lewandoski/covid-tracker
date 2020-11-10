42dc282626533aaeb3f3b092009cb86fa33e4465
Jan Lewandoski <jan.lewandoski@htdevelopers.com>
2020-11-10 14:47:19 +0100

    Fix search pipe

--- src/app/core/pipes/search.pipe.ts
+++ src/app/core/pipes/search.pipe.ts
@@ -1,18 +1,17 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Country } from '../models';
 
 @Pipe({
   name: 'search',
 })
 export class SearchPipe implements PipeTransform {
-  transform(value, keys: string, term: string) {
+  transform(value: Country[], keys: string, term: string) {
     if (!term) return value;
-    return (value || []).filter((item) =>
-      keys
-        .split(',')
-        .some(
-          (key) =>
-            item.hasOwnProperty(key) && new RegExp(term, 'gi').test(item[key])
-        )
+    term = term.toLowerCase();
+    return value.filter(
+      (item) =>
+        item.country.toLowerCase().includes(term) ||
+        item.continent.toLowerCase().includes(term)
     );
   }
 }
